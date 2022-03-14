2873c601dae6728f0131783a78f834d0fc864983
noornee <kendrickbon11@gmail.com>
2022-03-14 21:52:41 +0100

    more updates :)

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,10 +3,14 @@
 # Terminate already running bar instances
 killall -q polybar
 
-
-# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar main &
 
+polybar main --config=~/.config/polybar/config.ini &
+# polybar temp --config=~/.config/polybar/config.ini &
+
+
+echo "Polybar launched..."
