40a6c5822d6d8fb407cb0d811594cc9c383fd415
noornee <kendrickbon11@gmail.com>
2022-06-11 23:53:13 +0100

    added new plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,4 +1,4 @@
-return require('packer').startup(function()
+return require('packer').startup(function(use)
 	use({ 'wbthomason/packer.nvim' })
 	use({ 'preservim/nerdtree' })
 	use({ 'tjdevries/colorbuddy.nvim' })
@@ -9,22 +9,35 @@ return require('packer').startup(function()
 	use({ 'romgrk/barbar.nvim' })
 	use({ 'fatih/vim-go' })
 	use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
-	use({ 'neoclide/coc.nvim', branch = 'release' })
-	use({'preservim/nerdcommenter'})
+	--use({ 'neoclide/coc.nvim', branch = 'release' })
+	use({ 'preservim/nerdcommenter' })
 	use({ 'windwp/nvim-autopairs' })
 	use({ 'iamcco/markdown-preview.nvim' })
 	use({
 		"akinsho/toggleterm.nvim",
-		tag = 'v1.*', 
+		tag = 'v1.*',
 		config = function()
   			require("toggleterm").setup()
 		end
 	})
-	use({ 
+	use({
 		'lewis6991/gitsigns.nvim',
 		config = function()
 			require("gitsigns").setup()
 		end
 	})
 	use({ 'norcalli/nvim-colorizer.lua' })
+	use({
+		'neovim/nvim-lspconfig',
+		requires = {'williamboman/nvim-lsp-installer'}
+	})
+	use({
+		'hrsh7th/nvim-cmp',
+		requires = {
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-buffer',
+			'hrsh7th/cmp-path',
+		}
+	})
+	use({ 'L3MON4D3/LuaSnip' })
 end)
