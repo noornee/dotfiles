965978dbd2754421d8e0ee1250b03b3613b01e38
noornee <kendrickbon11@gmail.com>
2022-08-07 16:09:08 +0100

    added option for second monitor and refactored

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,7 +10,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 
 polybar main --config=~/.config/polybar/config.ini &
-#polybar external --config=~/.config/polybar/config.ini &
+
+if [[ $(xrandr | grep "DP-1-3 connected") ]];then
+	$HOME/.local/bin/screen.sh
+	polybar external --config=~/.config/polybar/config.ini &
+fi
 
 
 echo "Polybar launched..."
