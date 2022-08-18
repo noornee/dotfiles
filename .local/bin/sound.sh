cfbe37c5fae12569c8441d58d7d4168acd1e41da
noornee <kendrickbon11@gmail.com>
2022-08-18 07:38:04 +0100

    modified the scripts to use pulseaudio

--- .local/bin/sound.sh
+++ .local/bin/sound.sh
@@ -2,18 +2,17 @@
 
 function send_notification()
 {
-	vol_level=$(amixer get Master | grep '%'| cut -d '[' -f2 | cut -d '%' -f1)
+	vol_level=$(pactl get-sink-volume 0 | awk '{print $5}')
 	dunstify -i /home/noornee/.local/share/icons/volume.png -u low -r 9478 -t 1000 -h int:value:$vol_level "volume: $vol_level%"
 }
 
 case $1 in
 	"up")
-		amixer set 'Master' 10+
+		pactl set-sink-volume 0 +10%
 		send_notification $1;;
 
 	"down")
-		amixer set 'Master' 10-
-		#amixer -D default set Master 10-
+		pactl set-sink-volume 0 -10%
 		send_notification $1;;
 esac
 
