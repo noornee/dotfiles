094e6256652eabb136c7fa8202667206fb420fc4
noornee <kendrickbon11@gmail.com>
2022-08-20 23:17:53 +0100

    update script

--- .local/bin/update_dotfiles.sh
+++ .local/bin/update_dotfiles.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-rsync $HOME/{.zshenv,.Xresources} $HOME/.config/dotfiles
-
-rsync -hav $HOME/.local/bin/* $HOME/.config/dotfiles/.local/bin/
-
-rsync -hav $HOME/.local/share/icons/* $HOME/.config/dotfiles/.local/share/icons/
+cp $HOME/{.zshenv,.Xresources} $HOME/.config/dotfiles
+cp $HOME/.local/bin/* $HOME/.config/dotfiles/.local/bin/
+cp $HOME/.local/share/icons/* $HOME/.config/dotfiles/.local/share/icons/
 
 rsync -hav \
-	$HOME/.config/{alacritty,bspwm,lf,mpv,nvim,picom,polybar,sxhkd,tmux,zathura,zsh} \
+	$HOME/.config/{alacritty,bspwm,lf,mpv,nvim,picom,polybar,sxhkd,tmux,zathura,zsh,ncmpcpp} \
 	$HOME/.config/dotfiles/.config/
+
+cp $HOME/.config/mpd/mpd.conf $HOME/.config/dotfiles/.config/mpd
+
 
 
 
