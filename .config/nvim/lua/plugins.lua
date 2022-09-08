bed80f139b0bcbd94389166e33ee5fb028f9367e
noornee <kendrickbon11@gmail.com>
2022-09-08 21:18:29 +0100

    automatically install packer if not installed already

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,34 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system {
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	}
+	print("Installing packer.nvim\nplease close and reopen Neovim after its done")
+end
+
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	vim.notify("that didn't work")
+	return
+end
+
+-- Have packer use a popup window
+packer.init {
+	display = {
+		open_fn = function ()
+			return require("packer.util").float {border = "rounded"}
+		end,
+	},
+}
+
 return require('packer').startup(function(use)
 
 	use({ 'wbthomason/packer.nvim' })
