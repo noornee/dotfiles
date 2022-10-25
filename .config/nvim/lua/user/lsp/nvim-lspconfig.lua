7b44e8ca0be6a25d7fd7a73ac2685f6b27ec565c
noornee <noornee.011@gmail.com>
2022-10-25 19:36:01 +0100

    switched to default_capabilities since the other got deprecated

--- .config/nvim/lua/user/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/user/lsp/nvim-lspconfig.lua
@@ -1,5 +1,5 @@
 local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local on_attach = function()
 	local bufopts = { noremap = true, silent = true, buffer = 0 }
