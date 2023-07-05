0cdcf0ac88cc6636e45334bbd765a4f4855852f3
noornee <noornee.011@gmail.com>
2023-07-05 20:24:04 +0100

    fix: minor improvement? i think

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -7,13 +7,22 @@ IMG_NEW=$(echo $IMG_OLD | dmenu -i -p "rename:")
 
 
 
+# send notfication with notify-send
+function send_notification(){
+	notify-send --icon $1 "scrot" $1
+}
+
 if [[ $IMG_NEW == *.png ]]; then
 	# if the image name wasnt changed, move it to a new path
-	mv $HOME/$IMG_OLD $SAVE_PATH
+	NEW_PATH="$SAVE_PATH/$IMG_NEW"
+	mv $HOME/$IMG_OLD $NEW_PATH
+	send_notification $NEW_PATH
 else
 	# if the image name was changed, move it to a new path called "$SAVE_PATH/named" and rename
 	# if i rename an image then its cos i wanna use it for something so i choose to move it to a different directory for quick access
-	mv $HOME/$IMG_OLD "$SAVE_PATH/named/$IMG_NEW.png"
+	NEW_PATH="$SAVE_PATH/named/$IMG_NEW.png"
+	mv $HOME/$IMG_OLD $NEW_PATH
+	send_notification $NEW_PATH
 fi
 
 
